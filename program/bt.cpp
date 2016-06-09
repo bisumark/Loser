0764cf2c5cf39230984311bf75b9abc11969dcba
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-06-10 08:50:04 +0900

    edit bt node class to compile without c++11

--- program/bt.cpp
+++ program/bt.cpp
@@ -1,29 +1,55 @@
-#include <cmath>
 #include <iostream>
+#include <list>
+
 using namespace std;
 
 class Node{
+	public:
+		Node(){
+			RUNNING = 1;
+			FAILURE = -1;
+			SUCCESS = 0;
+		}
 	protected:
-		int RUNNING =  1;
-		int FAILURE = -1;
-		int SUCCESS =  0;
+		int RUNNING;
+		int FAILURE;
+		int SUCCESS;
 		virtual int run() = 0;
 };
+
+class CompositeNode : public Node{
+	protected:
+		list<Node*> children;
+	public:
+		void addChildren(Node* child){
+			children.push_back(child);
+		}
+		void check(){
+			cout << "ok2" << endl;
+		}
+};
  
-class Sequence : public Node{
+class Sequence : public CompositeNode{
 	public:
 		int run(){
-			return RUNNING;
+			return SUCCESS;
 		}
 };
 
-class Selector{
+class Selector : public CompositeNode{
 	public:
-		int run();
+		int run(){}
+};
+
+class Test: public Node{
+	public:
+		int run(){}
 };
 
 int main(){
-	Sequence ex;
-	cout << ex.run() << endl;
-	cout << "ok" << endl;
+	Sequence *root = new Sequence;
+	Test *test = new Test;
+	root->addChildren(test);
+
+	cout << "ok "<< root->run() << endl;
 }
